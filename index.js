b75cebfd91851eb02dd82ef1ef2c9374269de285
jasperwreed <jmixon.reed@gmail.com>
2020-10-16 10:13:55 -0700

    first test done

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      let val
+      for (val in collection) {
+        callback(collection[val])
+      }
+      return collection
     },
 
     map: function() {
@@ -25,3 +29,4 @@ const fi = (function() {
 })()
 
 fi.libraryMethod()
+fi.each([1,2,2], alert())
